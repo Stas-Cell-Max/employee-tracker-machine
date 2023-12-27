39fcc00ab5eca2e94f91d0afc99b2575f0413073
Stanislav Morozan <stanislav.morozan@gmail.com>
2023-12-27 14:55:54 -0500

    Set up an array of questions for main menu using Inquirer

---
+++ prompts/mainPrompts.js
@@ -0,0 +1,19 @@
+const mainMenuQuestions = [
+    {
+      type: 'list',
+      name: 'action',
+      message: 'What would you like to do?',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Exit'
+      ]
+    }
+  ];
+  
+  module.exports = mainMenuQuestions;
