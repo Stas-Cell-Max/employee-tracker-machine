9d762c0b096866ee6fb2d5f29d4f93d24a95dd6e
Stanislav Morozan <stanislav.morozan@gmail.com>
2023-12-28 18:15:51 -0500

    Implemented the  Database Interactions by Filling in Database Interaction

--- index.js
+++ index.js
@@ -30,46 +30,95 @@ const mainMenuQuestions = require('./prompts/mainPrompts');
  // Define the viewAllDepartments function (as a placeholder for now)
 function viewAllDepartments() {
     console.log('Viewing all departments...');
-    // After completing the action, show the main menu again
-    init();
+    
+    db.promise().query('SELECT * FROM departments')
+    .then(([rows]) => {
+      console.table(rows);
+    })
+    .catch(console.log)
+    .then(() => init());
 }     
 
 function viewAllRoles() {
     console.log('Viewing all roles...');
-    // Database interaction here
-    init();
+
+    db.promise().query('SELECT * FROM roles')
+    .then(([rows]) => {
+      console.table(rows);
+    })
+    .catch(console.log)
+    .then(() => init());
 }
 
 function viewAllEmployees() {
     console.log('Viewing all employees...');
-    // Database interaction here
-    init();
+
+    db.promise().query('SELECT * FROM employees')
+    .then(([rows]) => {
+      console.table(rows);
+    })
+    .catch(console.log)
+    .then(() => init());
 }
 
 function addDepartment() {
     console.log('Adding a new department...');
-    // Database interaction here
-    init();
+
+    inquirer.prompt({
+    name: 'newDepartment',
+    type: 'input',
+    message: 'What is the name of the new department?',
+  }).then(answer => {
+    db.promise().query('INSERT INTO departments (name) VALUES (?)', [answer.newDepartment])
+      .then(() => console.log(`Added ${answer.newDepartment} to the database`))
+      .catch(console.log)
+      .then(() => init());
+  });
 }
 
 function addRole() {
     console.log('Adding a new role...');
-    // Database interaction here
-    init();
+
+    inquirer.prompt({
+        name: 'newRole',
+        type: 'input',
+        message: 'What is the name of the new role?',
+      }).then(answer => {
+        db.promise().query('INSERT INTO roles (name) VALUES (?)', [answer.newRole])
+          .then(() => console.log(`Added ${answer.newRole} to the database`))
+          .catch(console.log)
+          .then(() => init());
+      });
 }
 
-function viewAllRoles() {
-    console.log('Viewing all roles...');
-    // Database interaction here
-    init();
+function addEmployee() {
+    console.log('Adding a new employee ...');
+
+    inquirer.prompt({
+        name: 'newEmployee',
+        type: 'input',
+        message: 'What is the name of the new role?',
+      }).then(answer => {
+        db.promise().query('INSERT INTO employee (name) VALUES (?)', [answer.newEmployee])
+          .then(() => console.log(`Added ${answer.newEmployee} to the database`))
+          .catch(console.log)
+          .then(() => init());
+      });
 }
-
-
 
 function updateEmployeeRole() {
     console.log('Updating an employee role...');
-    // Database interaction here
-    init();
+   
+    inquirer.prompt({
+        name: 'newEmployeeRole',
+        type: 'input',
+        message: 'What is the name of the employee new role ?',
+      }).then(answer => {
+        db.promise().query('INSERT INTO employee role (name) VALUES (?)', [answer.newEmployeeRole])
+          .then(() => console.log(`Added ${answer.newEmployeeRole} to the database`))
+          .catch(console.log)
+          .then(() => init());
+      });
 }
 
 // Define the exitApplication function
